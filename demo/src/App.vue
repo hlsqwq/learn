cb5852e12c9de2c286de69e5546bfe517d790976
hlsXD <3255446982@qq.com>
2025-12-28 21:21:03 +0800

    2025-12-28 21:21:03

---
+++ demo/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {  onMounted, provide, ref } from 'vue';
+
+
+
+// const res=ref([1,2,3,4,5,6,7,8,9])
+// let list=computed(()=>{
+//   return res.value.filter(i=>i>2)
+// })
+// list= computed({
+//   get:()=>{
+//   return res.value.filter(i=>i>2)
+    
+//   },
+//   set(val){
+//     res.value=res.value.map(i=>i+val)
+//   }
+// })
+
+// const res=ref({
+//   name:'zhangsan',
+//   age:18,
+//   work:'teacher'
+// })
+// watch([()=>res.value.age],(val)=>{
+//   console.log(val)
+// },{
+//   immediate:true,
+//   deep:true
+// })
+
+import Son from '@/components/son.vue'
+// const res=ref(100)
+// const flag=ref()
+
+// onMounted(()=>{
+//   console.log(flag.value.res);
+// })
+
+const res=ref(100)
+// provide('money',res)
+// provide('change',()=>{
+//   res.value++
+// })
+
+</script>
+<template>
+  <Son v-model="res"></Son>
+</template>
+<style lang="less" scoped></style>
---
+++ demo/src/components/son.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { inject, } from 'vue';
+
+  defineOptions({
+    name:'mySon'
+  })
+  
+
+  const res=defineModel()
+
+
+</script>
+<template>
+  <div>{{ res }}</div>
+  <button @click="res++">clikc</button>
+</template>
+<style lang="less" scoped>
+
+</style>
