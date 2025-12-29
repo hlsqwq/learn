16079462ec31072be3a31eba43f3e8f09dca0b59
hlsXD <3255446982@qq.com>
2025-12-29 14:44:41 +0800

    2025-12-29 14:44:41

---
+++ 25.12.29/demo/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+
+import {useCounterStore} from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+
+defineOptions({
+  name:'appIndex'
+})
+const res= useCounterStore()
+const {count}=storeToRefs(useCounterStore)
+res.func()
+
+</script>
+<template>
+  <div>{{res.count}}</div>
+</template>
+<style lang="less" scoped>
+
+</style>
---
+++ 25.12.29/demo/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+const app = createApp(App)
+app.use(createPinia().use(piniaPluginPersistedstate))
+
+app.mount('#app')
